56a41d848589c3ea5db3b6de2c27e9db30589528
Erik Jansson <erik.n.jansson@evry.com>
2020-07-07 00:56:12 +0200

    init2

---
+++ app/src/main/java/me/jfenn/alarmio/interfaces/AccelerometerListener.java
@@ -0,0 +1,8 @@
+package me.jfenn.alarmio.interfaces;
+
+public interface AccelerometerListener {
+
+    public void onAccelerationChanged(float x, float y, float z);
+
+    public void onShake(float force);
+}
